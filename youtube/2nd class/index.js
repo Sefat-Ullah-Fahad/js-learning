8e7339e1ab9c89aa34bbdd65216af6853d45b4b7
fahad <mdf780709@gmail.com>
2026-09-23 12:29:56 +0600

    learn else if conditions

--- youtube/2nd class/index.js
+++ youtube/2nd class/index.js
@@ -1,4 +1,18 @@
-// odd and evan
+// // odd and evan
 
-let num = 10
+// let num = 10
 
+let mode = "white"
+let color;
+
+if (mode === "dark"){
+    color = "black"
+} else if(mode === "white"){
+    color = "white"
+}else if(mode === "blur"){
+    color = "blur"
+} else {
+    color = "kisu na"
+}
+
+console.log(color)
