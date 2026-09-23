387370fdd4cdad11a5a0fd8d08a323f0e8d0af5f
fahad <mdf780709@gmail.com>
2026-09-23 20:07:08 +0600

    add task

--- youtube/2nd class/index.js
+++ youtube/2nd class/index.js
@@ -2,17 +2,29 @@
 
 // let num = 10
 
-let mode = "white"
-let color;
+// let mode = "white"
+// let color;
 
-if (mode === "dark"){
-    color = "black"
-} else if(mode === "white"){
-    color = "white"
-}else if(mode === "blur"){
-    color = "blur"
-} else {
-    color = "kisu na"
-}
+// if (mode === "dark"){
+//     color = "black"
+// } else if(mode === "white"){
+//     color = "white"
+// }else if(mode === "blur"){
+//     color = "blur"
+// } else {
+//     color = "kisu na"
+// }
 
-console.log(color)
+// console.log(color)
+
+
+// let age = 18
+
+
+// let result = age >= 18? "adult": "not adult"
+// console.log(result)
+
+
+
+// alert("hello")
+// prompt("hello")
---
+++ youtube/2nd class/task.js
@@ -0,0 +1,20 @@
+// let num = prompt("enter a number:")
+
+// if (num % 5 === 0) {
+//     console.log("is devided")
+// } else{
+//     console.log("this number not devided")
+// }
+
+
+
+
+let score = 1;
+
+if(score >=90 && score <= 100){
+    console.log("A")
+} else if(score >= 70 && score <= 89 ){
+    console.log("B")
+}else if(score >= 60 && score <= 69 ){
+    console.log("C")
+}
