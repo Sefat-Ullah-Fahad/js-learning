35730a23c2a49ea2cd20275df673ebaa0a9cd441
fahad <mdf780709@gmail.com>
2026-09-24 16:13:37 +0600

    add 3rd class

---
+++ youtube/3rd class/script.js
@@ -0,0 +1,19 @@
+// console.log("hello bagina")
+
+
+// for(let count =0; count <=99999; count ++){
+//     console.log(count,"hello bagina")
+// }
+// console.log("end of loop")
+
+
+
+
+let age = 0;
+let n = 10;
+
+for(let num = 1; num <= n; num ++){
+    age = age + num
+}
+
+console.log(age)
