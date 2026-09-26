e75ea87c2d3d4f5e7d812c62317d51a0f0bec953
fahad <mdf780709@gmail.com>
2026-09-26 12:58:26 +0600

    complete 2 task with js in loop

---
+++ youtube/3rd class/for-in.js
@@ -0,0 +1,11 @@
+const student = {
+    name: "fahad",
+    age: 19,
+    blod: "A+",
+    class: "college",
+    isPassd: true
+}
+
+for( let i in student){
+    console.log(student[i])
+}
---
+++ youtube/3rd class/task2.js
@@ -0,0 +1,16 @@
+let num = 4
+
+let userNum = prompt("enter of any number")
+
+while(userNum != num){
+  userNum =  prompt("this worng number , enter more number")   
+}
+console.log("number is curect")
+
+
+// let gameNum = 25;
+// let userNum = prompt("Guess the game number : ");
+
+// while (userNum !== gameNum) {
+//   userNum = prompt("You entered wrong number. Guess again : ");
+// }
